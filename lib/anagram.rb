b9d96f636449a5ee86d72a73736fb7cddc9a7ff4
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-22 17:21:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,14 @@ class Anagram
   end
 
   def match(array)
+    match_array = []
    array_of_words = array.sort
    array_of_words.each do |words|
-     @word == words
+     if @word == words
+       
+     else
+       
+     end
    end
   end
 
